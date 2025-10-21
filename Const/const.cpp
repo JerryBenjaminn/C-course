fee808108bc1a6410d3fc69c9275082eaeaa3533
JerryBenjaminn <jerrybenjaminn@users.noreply.github.com>
2025-10-21 17:49:24 +0300

    Const exercise

---
+++ Const/const.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+
+int main(){
+
+    //const keyword specifies that variable is constant
+    //tells the compiler to prevent anything from modifyin it
+    //(readonly)
+
+    const double PI = 3.14159;
+    double radius = 10;
+    double circumference = 2 * PI * radius;
+
+    std::cout << circumference << " cm" << "\n";
+
+    const int LIGHT_SPEED = 299792458;
+    const double DISTANCE_FROM_EARTH = 1.5 * pow(10, 11);
+    const double TIME_TO_TRAVEL = DISTANCE_FROM_EARTH / LIGHT_SPEED;
+    double time_to_travel_in_hours = TIME_TO_TRAVEL / 60;
+    std::cout << TIME_TO_TRAVEL << "s\n";
+    std::cout << time_to_travel_in_hours << std::endl;
+
+
+    return 0;
+
+}
