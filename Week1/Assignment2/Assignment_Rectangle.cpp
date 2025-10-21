5a57c91fe1d7d4fb930935afc6293ad8900ad375
JerryBenjaminn <jerrybenjaminn@users.noreply.github.com>
2025-10-21 23:31:07 +0300

    Assignment1 & 2

---
+++ Week1/Assignment2/Assignment_Rectangle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+    int width;
+    int heigth;
+
+    std::cout << "Type width: ";
+    std::cin >> width;
+
+    std::cout << "Type heigth: ";
+    std::cin >> heigth;
+    
+    double rectArea = width * heigth;
+    double rectPerimeter = (width * 2) + (heigth * 2);
+
+
+    std::cout << "Rectangle area: " << rectArea << "\n";
+    std::cout << "Rectangle perimeter: " << rectPerimeter;
+
+
+    return 0;
+}
