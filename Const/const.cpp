7ec7d6cc860c5b913c5a5b5af4c2fa3c5e3d380f
JerryBenjaminn <jerrybenjaminn@users.noreply.github.com>
2025-10-21 18:38:29 +0300

    Changed time to travel from hours to minutes

--- Const/const.cpp
+++ Const/const.cpp
@@ -17,9 +17,9 @@ int main(){
     const int LIGHT_SPEED = 299792458;
     const double DISTANCE_FROM_EARTH = 1.5 * pow(10, 11);
     const double TIME_TO_TRAVEL = DISTANCE_FROM_EARTH / LIGHT_SPEED;
-    double time_to_travel_in_hours = TIME_TO_TRAVEL / 60;
+    double time_to_travel_in_minutes = TIME_TO_TRAVEL / 60;
     std::cout << TIME_TO_TRAVEL << "s\n";
-    std::cout << time_to_travel_in_hours << std::endl;
+    std::cout << time_to_travel_in_minutes << std::endl;
 
 
     return 0;
