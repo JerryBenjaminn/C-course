6bc3dac3644f2dfb6f9d7693536c16eec2aa4fe5
JerryBenjaminn <jerrybenjaminn@users.noreply.github.com>
2025-10-27 14:52:38 +0200

    Fifth assignment

---
+++ Week1/Assignment5/Assignment_DividedWithThree.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+ {
+    int i = 1;
+    while (i <= 1000)
+    {
+        if (i % 3 == 0)
+        {
+          std::cout << i << "\n";
+        }
+
+        i++;
+    }
+
+    return 0;
+}
